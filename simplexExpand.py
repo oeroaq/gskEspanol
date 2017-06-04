fc651d30dd0b07cb57bd92de3d9c449a4660180e
Omar Roa <oeroaq@unal.edu.co>
2017-06-04 03:30:44 -0500

    maximizacion

--- simplexExpand.py
+++ simplexExpand.py
@@ -6,16 +6,124 @@ class simplexExpand(simplexVisitor):
     def __init__(self):
         self.memory = {}
         self.RCount = 0
+        self.SCount = 0
+        self.CVariables = []
         self.R = {}
+        self.RV = {}
         self.X = {}
         self.O = None
+        self.Variables = []
 
     def resetSelf(self):
         self.memory = {}
         self.RCount = 0
+        self.SCount = 0
+        self.CVariables = []
         self.R = {}
+        self.RV = {}
         self.X = {}
         self.O = None
+        self.Variables = []
+
+    def resolverSimplex(self):
+        self.R["0Z"] = self.X
+        for var in self.CVariables:
+            self.R["0Z"][var] *= -1
+        valor = (0, -1)
+        if self.O == simplexParser.MINI:
+            for i in range(self.RCount):
+                self.R["0Z"]["0R" + str(i)] = (0, -1)
+                valor = (0, 1)
+        for i in range(self.RCount):
+            fila = "0F" + str(i)
+            filaTmp = self.multiplicarFila(self.R[fila], valor)
+            self.R["0Z"] = self.sumarFilas(self.R["0Z"],filaTmp)
+        for v in self.Variables:
+            minimo = float("inf")
+            maximo = - float("inf")
+            columna = None
+            for item in self.Variables:
+                tupla = self.R["0Z"][item]
+                tmp = tupla[0] + tupla[1]
+                if self.O == simplexParser.MAXI:
+                    if tmp < minimo:
+                        minimo = tmp
+                        columna = item
+                else:
+                    if tmp > maximo:
+                        maximo = tmp
+                        columna = item
+            
+            minimo = float("inf")
+            maximo = - float("inf")
+            fila = None
+            for item in self.R:
+                if item != "0Z":
+                    if self.R[item][columna]:
+                        tmp = self.R[item]["0C"]/self.R[item][columna]
+                        if tmp > 0:
+                            if self.O == simplexParser.MAXI:
+                                if tmp < minimo:
+                                    minimo = tmp
+                                    fila = item
+                            else:
+                                if tmp > maximo:
+                                    maximo = tmp
+                                    fila = item
+            pivote = self.R[fila][columna]
+            self.R[fila] = self.multiplicarFila(self.R[fila], 1/pivote)
+            for item in self.R:
+                if item != fila:
+                    tuplaTmp = None
+                    if item != "0Z":
+                        tuplaTmp = -self.R[item][columna]
+                    else:
+                        tuplaTmp = (-self.R["0Z"][columna][0],-self.R["0Z"][columna][1])
+                    filaTmp = self.multiplicarFila(self.R[fila], tuplaTmp)
+                    self.R[item] = self.sumarFilas(self.R[item],filaTmp)
+        variables = {'OZ':None}
+        for item in self.Variables:
+            variables[item] = 0.0
+        for item in self.R:
+            for columna in self.R[item]:
+                valor = self.R[item]["0C"]
+                if columna == "0Z":
+                    variables[columna] = valor
+                elif self.R[item][columna] == 1:
+                    variables[columna] = valor
+        
+        print("Funcion:\n\tfuncion (%s) = %s\n"% (", ".join(self.Variables), variables["0Z"][0]))
+        print("Vaiables:")
+        for item in self.Variables:
+            if item != "0Z":
+                print("\tVariable %s = %s"% (item, variables[item]))
+        '''for item in self.R:
+            print(self.R[item])'''
+    def multiplicarFila(self, fila, valor):
+        resultado = {}
+        if type(valor) is tuple:
+            for item in fila:
+                resultado[item] = (valor[0] * fila[item],
+                                   valor[1] * fila[item])
+        else:
+            for item in fila:
+                resultado[item] = fila[item] * valor
+        return resultado
+
+    def sumarFilas(self, filaA, filaB):
+        resultado = {}
+        for item in filaA:
+            if type(filaA[item]) is tuple:
+                if type(filaB[item]) is tuple:
+                    resultado[item] = (filaA[item][0] + filaB[item][0], filaA[item][1] + filaB[item][1])
+                else:
+                    resultado[item] = (filaA[item][0] + filaB[item], filaA[item][1])
+            else:
+                if type(filaB[item]) is tuple:
+                    resultado[item] = (filaA[item] + filaB[item][0], filaB[item][1])
+                else:
+                    resultado[item] = filaA[item] + filaB[item]
+        return resultado
 
     def visitImprimirExpr(self, ctx):
         valor = self.visit(ctx.expresion())
@@ -35,7 +143,7 @@ class simplexExpand(simplexVisitor):
         if valor:
             self.memory[nombre] = valor
             return valor
-        print("Error:\n\tNo se pudo inicializar la variable '%s'." % nombre)
+        print("\tNo se pudo inicializar la variable '%s'." % nombre)
 
     def visitNumeros(self, ctx):
         return int(ctx.entero().getText())
@@ -49,17 +157,11 @@ class simplexExpand(simplexVisitor):
         if derecha and izquierda:
             if ctx.operador.type == simplexParser.MULT:
                 return izquierda * derecha
-            if derecha != 0:
+            if derecha != 0 and str(derecha) != "0":
                 return izquierda / derecha
-            var = ctx.expMAT(1).getText()
-            mensaje = ""
-            if str(var) != "0":
-                mensaje = "Error: \n\tNo se puede dividir por cero."
-            else:
-                mensaje = "Error: \n\tLa evaluacion de la expresion o variable '%s' no puede ser cero." % var
-            print(mensaje)
+        print("Error:\n\tLa evaluacion de la expresion: '%s' no se puede calcular." % (
+            ctx.expMAT(0).getText(), ctx.expMAT(1).getText()))
         return None
-
 
     def visitOperSuma(self, ctx):
         izquierda = self.visit(ctx.expMAT(0))
@@ -82,7 +184,7 @@ class simplexExpand(simplexVisitor):
 
     def visitMenosExpMAT(self, ctx):
         numero = self.visit(ctx.expMAT())
-        if numero:
+        if numero or numero == 0:
             return -numero
         return numero
 
@@ -144,29 +246,119 @@ class simplexExpand(simplexVisitor):
         return None
 
     def visitProblemasExpresados(self, ctx):
-        self.visit(ctx.problema())
+        if self.visit(ctx.problema()) != -1:
+            self.resolverSimplex()
 
     def visitProblemas(self, ctx):
         if self.visit(ctx.funcionTrans()) != -1:
             if self.visit(ctx.restriccion()) != -1:
-                print(self.X)
-                print(self.R)
+                if self.visit(ctx.restricionVariable()) != -1:
+                    return 0
         return -1
 
     '''
         Define el arbol para la definicion de las restricciones
     '''
 
+    def visitRestriccionesVariables(self, ctx):
+        for desigualdad in ctx.desigualdadVariable():
+            V, R = self.visit(desigualdad)
+            if V and R:
+                self.RV[V] = R
+            else:
+                self.resetSelf()
+                return -1
+        variables = []
+        for var in self.Variables:
+            if var not in self.CVariables:
+                variables.append(var)
+        if not len(variables):
+            return 0
+        print("Error:\n\tNo se definieron las condiciones para las variables: '%s'." %
+              "', '".join(variables))
+        return -1
+
+    def visitDesigualdadesVariables(self, ctx):
+        variable = self.visit(ctx.variable())
+        if variable in self.Variables:
+            if variable not in self.CVariables:
+                self.CVariables.append(variable)
+            else:
+                print(
+                    "Cuidado!:\n\tLa condicion para la variable '%s' ya habia sido definida, se sobreescribe." % variable)
+            restriccion = {}
+            if ctx.tipo.type == simplexParser.MAYOR:
+                restriccion["tipo"] = ">"
+            elif ctx.tipo.type == simplexParser.MENOR:
+                restriccion["tipo"] = "<"
+            else:
+                restriccion["tipo"] = "="
+            numero = self.visit(ctx.expMAT())
+            if numero or numero == 0:
+                restriccion["0C"] = numero
+                return variable, restriccion
+            else:
+                print(numero)
+                print("\tEn la restriccion numero %s." % (self.RCount + 1))
+                return None, None
+        print(self.X[variable])
+        print("Error:\n\tLa variable '%s' no esta definida en la funcion." % variable)
+        print("\tEn la condicion numero %s" % (self.RCount + 1))
+        return None, None
+
+    def visitDesigualdadesVariablesMayorIgual(self, ctx):
+        variable = self.visit(ctx.variable())
+        if variable in self.Variables:
+            if variable not in self.CVariables:
+                self.CVariables.append(variable)
+            else:
+                print(
+                    "Cuidado!:\n\tLa condicion para la variable '%s' ya habia sido definida, se sobreescribe." % variable)
+            restriccion = {}
+            restriccion["tipo"] = ">="
+            numero = self.visit(ctx.expMAT())
+            if numero or numero == 0:
+                restriccion["0C"] = numero
+                return variable, restriccion
+            else:
+                print("\tEn la restriccion numero %s." % (self.RCount + 1))
+                return None, None
+        return None, None
+
+    def visitDesigualdadesVariablesMenorIgual(self, ctx):
+        variable = self.visit(ctx.variable())
+        if variable in self.Variables:
+            if variable not in self.CVariables:
+                self.CVariables.append(variable)
+            else:
+                print(
+                    "Cuidado!:\n\tLa condicion para la variable '%s' ya habia sido definida, se sobreescribe." % variable)
+            restriccion = {}
+            restriccion["tipo"] = "<="
+            numero = self.visit(ctx.expMAT())
+            if numero or numero == 0:
+                restriccion["0C"] = numero
+                return variable, restriccion
+            else:
+                print("\tEn la restriccion numero %s." % (self.RCount + 1))
+                return None, None
+        return None, None
+
     def visitRestricciones(self, ctx):
         for noigualdad in ctx.desigualdad():
             restriccion = self.visit(noigualdad)
             if restriccion != -1:
-                variable = "0X" + str(self.RCount)
-                self.X[variable] = 0
-                self.R["0R" + str(self.RCount)] = restriccion
+                R = "0R" + str(self.RCount)
+                S = "0S" + str(self.SCount)
+                self.X[R] = (0, 1)
+                self.X[S] = 0
+                for item in self.R:
+                    self.R[item][R] = 0
+                    self.R[item][S] = 0
+                self.R["0F" + str(self.RCount)] = restriccion
                 self.RCount += 1
+                self.SCount += 1
             else:
-                print("\tEn la restriccion numero %s." % (self.RCount+1))
                 self.resetSelf()
                 return -1
         return 0
@@ -174,59 +366,72 @@ class simplexExpand(simplexVisitor):
     def visitDesigualdades(self, ctx):
         restriccion = self.polinomioRestriccion(ctx)
         if restriccion != -1:
-            variable = "0X" + str(self.RCount)
+            R = "0R" + str(self.RCount)
+            S = "0S" + str(self.SCount)
             if ctx.tipo.type == simplexParser.MAYOR:
-                restriccion[variable] = -1
+                restriccion[R] = 1
+                restriccion[S] = -1
             elif ctx.tipo.type == simplexParser.MENOR:
-                restriccion[variable] = 1
+                restriccion[R] = 0
+                restriccion[S] = 1
             else:
-                restriccion[variable] = 0
+                restriccion[R] = 1
+                restriccion[S] = 0
             numero = self.visit(ctx.expMAT())
-            if numero:
-                restriccion["0C"] += numero 
+            if numero or numero == 0:
+                restriccion["0C"] += numero
             else:
+                print("\tEn la restriccion numero %s." % (self.RCount + 1))
                 return -1
         return restriccion
 
     def visitDesigualdadesMayorIgual(self, ctx):
         restriccion = self.polinomioRestriccion(ctx)
         if restriccion != -1:
-            variable = "0X" + str(self.RCount)
-            restriccion[variable] = -1
+            R = "0R" + str(self.RCount)
+            S = "0S" + str(self.SCount)
+            restriccion[R] = 1
+            restriccion[S] = -1
             numero = self.visit(ctx.expMAT())
-            if numero:
-                restriccion["0C"] += numero 
+            if numero or numero == 0:
+                restriccion["0C"] += numero
             else:
+                print("\tEn la restriccion numero %s." % (self.RCount + 1))
                 return -1
         return restriccion
 
     def visitDesigualdadesMenorIgual(self, ctx):
         restriccion = self.polinomioRestriccion(ctx)
         if restriccion != -1:
-            variable = "0X" + str(self.RCount)
-            restriccion[variable] = 1
+            R = "0R" + str(self.RCount)
+            S = "0S" + str(self.SCount)
+            restriccion[R] = 0
+            restriccion[S] = 1
             numero = self.visit(ctx.expMAT())
-            if numero:
-                restriccion["0C"] += numero 
+            if numero or numero == 0:
+                restriccion["0C"] += numero
             else:
+                print("\tEn la restriccion numero %s." % (self.RCount + 1))
                 return -1
         return restriccion
-
 
     def polinomioRestriccion(self, ctx):
         polinomio = self.visit(ctx.polinomio())
-        restriccion = {}
-        for X in self.X:
-            restriccion[X] = 0
-        for monomio in polinomio:
-            if monomio in self.X:
-                restriccion[monomio] = polinomio[monomio]
-            else:
-                print("Error en la restriccion %s: \n\t La variable '%s' no se encuentra definida en la funcion" % (
-                    (self.RCount + 1), monomio))
-                self.resetSelf()
-                return -1
-        return restriccion
+        if polinomio:
+            restriccion = {}
+            for X in self.X:
+                restriccion[X] = 0
+            for monomio in polinomio:
+                if monomio in self.X:
+                    restriccion[monomio] = polinomio[monomio]
+                else:
+                    print("Error en la restriccion %s: \n\tLa variable '%s' no se encuentra definida en la funcion" % (
+                        (self.RCount + 1), monomio))
+                    self.resetSelf()
+                    return -1
+            return restriccion
+        print("\tEn la restriccion numero %s." % (self.RCount + 1))
+        return -1
     '''
         Define el arbol para la definicion de la funion a maximizar o minimizar
     '''
@@ -247,17 +452,20 @@ class simplexExpand(simplexVisitor):
                     self.X[monomio] = polinomio[monomio]
                 else:
                     self.resetSelf()
-                    print("Error en la declaracion de la funcion a maximizar: \n\t La variable '%s' no se encuentra definida en la funcion" % monomio)
+                    print(
+                        "Error en la declaracion de la funcion a maximizar: \n\t La variable '%s' no se encuentra definida en la funcion" % monomio)
                     return -1
             return 0
         print("\tEn la declaracion de la funcion a maximizar.")
         return -1
 
     def visitDefinirFuncion(self, ctx):
-        self.X = {}
+        self.resetSelf()
+        self.X["0Z"] = 1
         for var in ctx.variable():
             variable = self.visit(var)
             self.X[variable] = 0
+            self.Variables.append(variable)
         self.X["0C"] = 0
         return 0
 
--- test.b
+++ test.b
@@ -1,12 +1,35 @@
-d=21
-max:
-    funcion (a,b,c,d)= 12a -b -0.1a  + (d**12)c + (d)c +(d)+ d + 1
+##
+min:
+    fun (a,b,c)= (-5)a-4b
 restringir:
-    12a -b -0.1a > (d)
-    13c - 21 >= (25**3)
-    12a -b -0.1a < (d/2)
-    13a - 21 <= (25**1)
+    2a+2b <= 1
+    6a+3b <= 1
+    5a+10b <= 1
 con:
-    a>0
-    b<=(d*2)
+    a >= 0
+    b >= 0
+    c >= 0
+res
+##
+max:
+    fun (a,b,c)= 2a+3b-5c
+restringir:
+    a+b+c = 7
+    2a-5b+c >= 10
+con:
+    a >= 0
+    b >= 0
+    c >= 0
+res
+
+maximizar:
+    fun (a,b,c)= 315a+110b+50c
+restringir:
+    15a+2b+c <= 200
+    7.5a+3b+c >= 150
+    5a+2b+c = 120
+con:
+    a >= 0
+    b >= 0
+    c >= 0
 res
